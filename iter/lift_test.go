fd12cffe6efcd8930eef1f6d14d9bc7576b73e63
weakptr <weakptr@outlook.com>
2023-04-01 02:34:28 +0800

    test: add benchmark lift vs ordinary for loop

--- iter/lift_test.go
+++ iter/lift_test.go
@@ -1,8 +1,10 @@
 package iter
 
 import (
+	"crypto/rand"
 	"fmt"
 	assert2 "github.com/nnnewb/battery/internal/assert"
+	"io"
 	"testing"
 )
 
@@ -23,4 +25,31 @@ func TestLift(t *testing.T) {
 
 func TestLiftEmpty(t *testing.T) {
 	assert2.Assert(t, !Lift([]int{}).Next())
+}
+
+// BenchmarkLiftIteration 验证下 Lift 等 Generator 形式创建的迭代器对比下标访问的性能损失有多大
+//
+//	cpu: AMD Ryzen 7 4800H with Radeon Graphics
+//	BenchmarkLiftIteration/iteration_by_lift-16               1000000000 0.3121 ns/op  0 B/op 0 allocs/op
+//	BenchmarkLiftIteration/iteration_by_ordinary_for-range-16 1000000000 0.02552 ns/op 0 B/op 0 allocs/op
+func BenchmarkLiftIteration(b *testing.B) {
+	data := make([]byte, 1024*1024*100)
+	n, err := io.ReadAtLeast(rand.Reader, data, len(data))
+	if err != nil {
+		b.Fatal(err)
+	}
+	if n < len(data) {
+		b.Fatal("can not read enough random bytes")
+	}
+
+	b.ResetTimer()
+	b.Run("iteration by lift", func(b *testing.B) {
+		it := Lift(data)
+		for it.Next() {
+		}
+	})
+	b.Run("iteration by ordinary for loop", func(b *testing.B) {
+		for i := 0; i < len(data); i++ {
+		}
+	})
 }

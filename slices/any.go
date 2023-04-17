fd8a1b26b4fa5c652b3fb633f2295881ce944c86
nnnewb <weakptr@outlook.com>
2023-04-17 14:57:34 +0800

    test: add slices.any_test.go

--- slices/any.go
+++ slices/any.go
@@ -2,6 +2,8 @@ package slices
 
 import "github.com/nnnewb/battery/iter"
 
+// Any 测试切片元素是否有任意一个元素满足条件，有则返回 true 。
+// 切片为空或没有满足条件的元素则返回 false 。
 func (s Slice[T]) Any(predicate func(T) bool) bool {
 	return iter.Any(iter.Lift(s), predicate)
 }

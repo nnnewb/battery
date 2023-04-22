a1f723e07da7d01be0f7ec136615235aea6846b1
nnnewb <weakptr@outlook.com>
2023-04-22 16:04:29 +0800

    refactor: change Keys/Values signature

--- maps/keys.go
+++ maps/keys.go
@@ -1,7 +1,9 @@
 package maps
 
+import "github.com/nnnewb/battery/slices"
+
 // Keys returns the keys of the map m. The keys will be in an indeterminate order.
-func Keys[M ~map[K]V, K comparable, V any](m M) []K {
+func Keys[M ~map[K]V, K comparable, V any](m M) slices.Slice[K] {
 	keys := make([]K, 0, len(m))
 	for k := range m {
 		keys = append(keys, k)
--- maps/values_test.go
+++ maps/values_test.go
@@ -1,7 +1,6 @@
 package maps
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -10,7 +9,9 @@ func TestValues(t *testing.T) {
 	m := map[string]int{"a": 1, "b": 2, "c": 3}
 	expected := []int{1, 2, 3}
 	result := Values(m)
-	if !reflect.DeepEqual(result, expected) {
-		t.Errorf("Values(%v) = %v; expected %v", m, result, expected)
+	for _, v := range expected {
+		if !result.Contains(v, func(i int, i2 int) bool { return i == i2 }) {
+			t.Errorf("Values(%v) = %v; expected %v", m, result, expected)
+		}
 	}
 }

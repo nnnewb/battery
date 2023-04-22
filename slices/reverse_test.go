01c463095abe5b584da398fd87a61853ff2dbe64
nnnewb <weakptr@outlook.com>
2023-04-22 14:40:44 +0800

    feat: add Slice.Reverse method

---
+++ slices/reverse_test.go
@@ -0,0 +1,34 @@
+package slices
+
+import "testing"
+
+// TestReverse tests the Reverse function
+func TestReverse(t *testing.T) {
+	// Test with an empty slice
+	s := make(Slice[int], 0)
+	reversed := s.Reverse()
+	if len(reversed) != 0 {
+		t.Errorf("Reverse of an empty slice should be an empty slice")
+	}
+
+	// Test with a slice of length 1
+	s = Slice[int]{1}
+	reversed = s.Reverse()
+	if len(reversed) != 1 || reversed[0] != 1 {
+		t.Errorf("Reverse of a slice of length 1 should be the same slice")
+	}
+
+	// Test with a slice of length 2
+	s = Slice[int]{1, 2}
+	reversed = s.Reverse()
+	if len(reversed) != 2 || reversed[0] != 2 || reversed[1] != 1 {
+		t.Errorf("Reverse of a slice of length 2 should be [2, 1]")
+	}
+
+	// Test with a slice of length 3
+	s = Slice[int]{1, 2, 3}
+	reversed = s.Reverse()
+	if len(reversed) != 3 || reversed[0] != 3 || reversed[1] != 2 || reversed[2] != 1 {
+		t.Errorf("Reverse of a slice of length 3 should be [3, 2, 1]")
+	}
+}

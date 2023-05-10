89218944be4221d3cb2484921527c2a1fd13204a
weak_ptr <weak_ptr@outlook.com>
2023-05-10 16:36:22 +0800

    chore: simply rename parameter f to predicate

--- iter/filter.go
+++ iter/filter.go
@@ -1,12 +1,12 @@
 package iter
 
 // Filter 创建一个迭代器，过滤出符合条件的元素。
-func Filter[T any](iterator Iterator[T], f func(T) bool) Iterator[T] {
+func Filter[T any](iterator Iterator[T], predicate func(T) bool) Iterator[T] {
 	return Generator(func() GenFunc[T] {
 		return func() (T, bool) {
 			var t T
 			for iterator.Next() {
-				if f(iterator.Value()) {
+				if predicate(iterator.Value()) {
 					return iterator.Value(), true
 				}
 			}
